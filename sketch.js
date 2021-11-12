488c56c945e49cfccf5df1615fe3590aaf0563d4
twinklemistry21 <82593221+twinklemistry21@users.noreply.github.com>
2021-11-12 10:56:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage, obstacle1Image, obstacle2Image; 
+var blastImage;                   //C42// SA
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles; 
+var cars = [];
+
+function preload() {
+  backgroundImage = loadImage("background.png");
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  track = loadImage("track.jpg");
+  fuelImage = loadImage("fuel.png");
+  powerCoinImage = loadImage("goldCoin.png");
+  lifeImage = loadImage("life.png");
+  obstacle1Image = loadImage("obstacle1.png"); 
+  obstacle2Image = loadImage("obstacle2.png"); 
+  blastImage = loadImage("blast.png"); //C42 //SA
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
